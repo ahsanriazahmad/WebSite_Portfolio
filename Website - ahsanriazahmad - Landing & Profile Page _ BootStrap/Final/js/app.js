e99a06958a852e531e36daeb97bf9af29eb53c29
ahsanriazahmad <ahsanriazahmad1@gmail.com>
2022-01-16 20:57:56 -0800

    Matching Folders from Desktop to GitHuB

---
+++ Website - ahsanriazahmad - Landing & Profile Page _ BootStrap/Final/js/app.js
@@ -0,0 +1,18 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+/*function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}*/
